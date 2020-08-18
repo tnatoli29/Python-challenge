200638f0c8e35197a9df28883c886fa990b1b28d
Thomas <tpnats@gmail.com>
2020-08-18 12:11:20 -0400

    5th commit with total months, amount, and average

--- PyBank/Code/Main.py
+++ PyBank/Code/Main.py
@@ -3,13 +3,55 @@
 import os
 import csv
 
+#define ou variables
+total_months = 0
+total_amount = 0
+greatest_increase = 0
+best_month = 0
+greatest_decrease = 0
+worst_month = 0
+monthly_change =[]
+month_count = []
+
 csv_budget = os.path.join("..", "Resources", "budget_data.csv")
 
 with open(csv_budget) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
+#Identify the header
     budget_header = next(csvreader)
-    print(f"Budget Header: {budget_header}")
+    row = next(csvreader)
+
+#set variables within the "with" function
+    total_months += 1
+    total_amount += int(row[1])
+    previous_month = int(row[1])
 
     for row in csvreader:
-        print(row)
+        #calculate the total number of months
+        total_months += 1
+        #calculate the total amount of profits and losses
+        total_amount += int(row[1])
+        #calculate the changing from one month the the previous
+        profit_change = int(row[1]) - previous_month
+        monthly_change.append(profit_change)
+        previous_month = int(row[1])
+        #month_count.append(row[0])
+
+
+
+
+
+    average_monthly_change = (sum(monthly_change) / len(monthly_change))
+    average_monthly_change = round(average_monthly_change, 2)
+
+
+
+
+print(f"Total Months: {total_months}")
+print(f"Total Amount: ${total_amount}")
+print(f"Average Change: ${average_monthly_change:}")
+#print(f"Greatest Increase: {}")
+#print(f"Greatest Decrease: {}")
+
+
