498a667cceac8a80eb0d636f18086d0cd22183a0
Thomas <tpnats@gmail.com>
2020-08-18 10:48:20 -0400

    4th commit with CSV working

--- PyBank/Code/Main.py
+++ PyBank/Code/Main.py
@@ -1,14 +1,15 @@
 
-#First import the os and cvs modules allowing us to file paths across systems and to read csv files
+# First import the os and cvs modules allowing us to file paths across systems and to read csv files
 import os
 import csv
 
-#This allows us to pull data from another file
-budget_path = os.path.join("..", "Resources","budget_data.csv")
+csv_budget = os.path.join("..", "Resources", "budget_data.csv")
 
-#CSV module
-with open(budget_path) as csvfile:
+with open(csv_budget) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-    csvreader = csv.reader(csvfile)
-    print(csvreader)
+    budget_header = next(csvreader)
+    print(f"Budget Header: {budget_header}")
 
+    for row in csvreader:
+        print(row)
